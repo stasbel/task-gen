a57491396dd675fdcc50f868a7d20624c990a268
Stanislav Belyaev <stasbelyaev96@gmail.com>
2018-03-19 18:23:53 +0300

    Adjust env to google collab to work

--- utils.py
+++ utils.py
@@ -6,7 +6,8 @@ def hyperdash(name):
     def decorate(func):
         @wraps(func)
         def wrapper(*args, **kwargs):
-            exp = Experiment(name, capture_io=False)
+            exp = Experiment(name, capture_io=False,
+                             api_key_getter=lambda: 'mB3wTK1XXivrCFp4HpnX/KDUFT/0az3+W8BhLSF+Vdg=')
             kwargs['exp'] = exp
             result = func(*args, **kwargs)
             exp.end()
