de926d0baf06b9bf8bc03afa60e0c278dd4f6a3e
Stanislav Belyaev <stasbelyaev96@gmail.com>
2018-04-02 03:54:26 +0300

    Complete text validator

--- data/validator.py
+++ data/validator.py
@@ -4,15 +4,20 @@ import spacy
 from langdetect import detect as detect_lang
 from spacy_cld import LanguageDetector
 
+ALPHAS = ascii_lowercase + ascii_lowercase.upper()
+
 
 class Validator:
-    def __init__(self, language='en', len_b=(25, 3000), arate=0.8):
+    """Simple functor, perfoeming plausible text check."""
+
+    def __init__(self, language='en', len_b=(25, 3000), arate=0.75):
         assert language == 'en', "Only support en for now"
+        assert 0 < arate <= 1, ""
 
         self.language = language
         self.nlp = spacy.load(language)
         self.nlp.add_pipe(LanguageDetector())
-        self.len_lb, self._len_up = len_b
+        self.len_lb, self.len_up = len_b
         self.arate = arate
 
         self.checks = [
@@ -22,7 +27,7 @@ class Validator:
         ]
 
     def __call__(self, word):
-        pass
+        return all(check(word) for check in self.checks)
 
     def _len_check(self, text):
         return self.len_lb <= len(text) <= self.len_up
@@ -32,7 +37,7 @@ class Validator:
                and self._is_infer_langdetect(text)
 
     def _is_infer_lang_spacy_cld(self, text):
-        languages = self.nlp(text)._.languages[0]
+        languages = self.nlp(text)._.languages
         return len(languages) and languages[0] == self.language
 
     def _is_infer_langdetect(self, text):
@@ -48,5 +53,5 @@ class Validator:
         return all(31 < ord(c) < 128 for c in text)
 
     def _is_good_arate(self, text):
-        m = sum(bool(c in ascii_lowercase) for c in text.lower())
+        m = sum(bool(c in ALPHAS) for c in text)
         return m / len(text) >= self.arate
