e12817e587ff14eedead43c9b2412a896342eae3
Stanislav Belyaev <stasbelyaev96@gmail.com>
2018-05-05 08:52:37 +0300

    Text VAE with sampling prior code complete

--- text-vae/corpus.py
+++ text-vae/corpus.py
@@ -11,46 +11,84 @@ class Corpus(abc.ABC):
 
     @abc.abstractmethod
     def vocab(self, name):
+        """Gets vocab instance by var name"""
         pass
 
     @abc.abstractmethod
-    def make_batcher(self, mode, split, batch_size):
+    def batcher(self, mode, split, **kwargs):
+        """Make batcher generator"""
+        pass
+
+    @abc.abstractmethod
+    def reverse(self, example, name):
+        """Reverse given example to human-readable form"""
         pass
 
 
 class SSTCorpus(Corpus):
-    def __init__(self, args, device):
-        self.n_batch = args.n_batch
-        self.n_len = args.n_len
-        self.n_vocab = args.n_vocab
-        self.d_emb = args.d_emb
-        self.device = device
+    def __init__(self, **kwargs):
+        self.n_batch = kwargs['n_batch']
+        self.n_len = kwargs['n_len']
+        self.n_vocab = kwargs['n_vocab']
+        self.d_emb = kwargs['d_emb']
+        self.device = kwargs['device']
 
         self._load_data()
 
     def vocab(self, name):
-        return getattr(self, name).vocab
+        vocab = getattr(self, name).vocab
+        # vocab.vectors = vocab.vectors.to(self.device)
+        return vocab
 
-    def make_batcher(self, mode, split, n_batch=None, device=None):
+    def batcher(self, mode, split, *, n_batch=None, device=None, n_iter=None):
         n_batch = n_batch or self.n_batch
         device = device or self.device
-        b_iter = self._make_b_iter(split, n_batch, device)
+        split_instance = self._choose_split(split)
+        n_iter = n_iter or len(split_instance)
+        b_iter = data.BucketIterator(
+            split_instance,
+            n_batch,
+            train=(split == 'train'),
+            device=device  # also explicitly due to torchtext bug
+        )
 
         if mode == 'unlabeled':
+            i_iter = 0
             for batch in b_iter:
                 if batch.batch_size == n_batch:
-                    yield batch.text
+                    i_iter += 1
+                    yield batch.text.to(device)
+                if i_iter == n_iter:
+                    break
         elif mode == 'labeled':
+            i_iter = 0
             for batch in b_iter:
                 if batch.batch_size == n_batch:
-                    yield batch.text, batch.label
+                    i_iter += 1
+                    yield batch.text.to(device), batch.label.to(device)
+                if i_iter == n_iter:
+                    break
         else:
             raise ValueError(
                 "Invalid mode, should be one of the ('unlabeled', 'labeled')"
             )
 
+    def reverse(self, example, name='x'):
+        # sent = ' '.join(corpus.vocab('x').itos[w] \
+        #                 for w in model.sample_sentence(device=device) \
+        #                 if w > 3)
+
+        if name == 'x':
+            return self.x.reverse(example.unsqueeze(0))[0]
+        elif name == 'y':
+            return self.y.itos[example]
+        else:
+            raise ValueError(
+                "Invalid name, should be one of the ('x', 'y')"
+            )
+
     def _load_data(self):
-        self.X = data.ReversibleField(
+        self.x = data.ReversibleField(
             init_token='<bos>',
             eos_token='<eos>',
             fix_length=self.n_len,
@@ -70,25 +108,17 @@ class SSTCorpus(Corpus):
             return len(e.text) <= self.n_len and e.label != 'neutral'
 
         self.train, self.val, self.test = datasets.SST.splits(
-            self.X,
+            self.x,
             self.y,
             fine_grained=False,
             train_subtrees=False,
             filter_pred=filter_pred
         )
 
-        self.X.build_vocab(self.train,
+        self.x.build_vocab(self.train,
                            max_size=self.n_vocab,
                            vectors=GloVe('6B', dim=self.d_emb))
         self.y.build_vocab(self.train)
-
-    def _make_b_iter(self, split, n_batch, device):
-        return data.BucketIterator(
-            self._choose_split(split),
-            n_batch,
-            train=(split == 'train'),
-            device=device
-        )
 
     def _choose_split(self, split):
         if split == 'train':
