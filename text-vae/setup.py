eb19ba9462ab74927829652aa6276763e90c87e1
Stanislav Belyaev <stasbelyaev96@gmail.com>
2018-05-18 18:20:23 +0300

    Finish text vae

---
+++ text-vae/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='text-vae',
+    version='0.0.1',
+    python_requires='>=3.6.0',
+    author='Stanislav Belyaev',
+    author_email='stasbelyaev96@gmail.com'
+)
